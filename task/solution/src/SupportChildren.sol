bd148064018669c5ed544c3781bc83a84c31b2c6
vaniiiii <ivandzambas@gmail.com>
2024-08-04 12:50:38 +0200

    Initialized foundry proj, added draft files

---
+++ task/solution/src/SupportChildren.sol
@@ -0,0 +1,6 @@
+pragma solidity 0.8.26;
+
+import "./interfaces/ISupportChildren.sol";
+
+abstract contract SupportChildren is ISupportChildren {
+}
---
+++ task/solution/src/interfaces/IERC20.sol
@@ -0,0 +1,9 @@
+pragma solidity 0.8.26;
+
+interface IERC20 {
+    function transferFrom(
+        address from,
+        address to,
+        uint256 value
+    ) external returns (bool);
+}
