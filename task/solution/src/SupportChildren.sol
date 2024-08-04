9f10c7b9e13b6f4ddffed84cb0ce337deb9878f0
vaniiiii <ivandzambas@gmail.com>
2024-08-04 13:28:20 +0200

    Added draft implementation

--- task/solution/src/SupportChildren.sol
+++ task/solution/src/SupportChildren.sol
@@ -1,6 +1,135 @@
 pragma solidity 0.8.26;
 
-import "./interfaces/ISupportChildren.sol";
+import {ISupportChildren} from "./interfaces/ISupportChildren.sol";
+import {IERC20} from "./interfaces/IERC20.sol";
 
-abstract contract SupportChildren is ISupportChildren {
+contract SupportChildren is ISupportChildren {
+    uint256 index;
+    mapping(uint256 campaignId => Campaign) public campaigns;
+
+    function isCampaignActive(
+        uint256 _campaignId
+    ) public view override returns (bool) {
+        return _isCampaignActive(_campaignId);
+    }
+
+    function getCampaign(
+        uint256 _campaignId
+    ) public view override returns (Campaign memory) {
+        return campaigns[_campaignId];
+    }
+
+    function createCampaign(
+        address payable _beneficiary,
+        uint256 _endTimestamp,
+        string calldata uri,
+        address wantToken,
+        uint256 _hardCap
+    ) external {
+        _checkCampaignCreationParameters(_beneficiary, _endTimestamp, _hardCap);
+
+        uint256 campaignId = index;
+        Campaign memory campaign = Campaign(
+            _endTimestamp,
+            _beneficiary,
+            uri,
+            wantToken,
+            _hardCap,
+            0,
+            campaignId
+        );
+
+        campaigns[campaignId] = campaign;
+        index = campaignId + 1;
+
+        emit CampaignCreated(campaignId, campaign);
+    }
+
+    function donateETH(uint256 _campaignId) external payable {
+        if (msg.value == 0) {
+            revert SupportChildren__EthAmountMustBeGreaterThanZero();
+        }
+        // @dev, no need to check if campaign exist because endTimestamp will be 0 if campaign does not exist, which is always less than block.timestamp
+        if (!_isCampaignActive(_campaignId)) {
+            revert SupportChildren__CampaignNotActive();
+        }
+
+        Campaign memory campaign = campaigns[_campaignId];
+        if (campaign.wantToken != address(0)) {
+            revert SupportChildren__CampaignWantTokenIsNotETH();
+        }
+        if (campaign.receivedAmount + msg.value > campaign.hardCap) {
+            revert SupportChildren__CampaignCapIsReached();
+        }
+
+        // Update the received amount, @research CEI pattern
+        campaigns[_campaignId].receivedAmount += msg.value;
+
+        (bool sent /* bytes memory data */, ) = campaign.beneficiary.call{
+            value: msg.value
+        }("");
+        if (!sent) {
+            revert SupportChildren__FailedToSendEther();
+        }
+
+        emit Donation(_campaignId, msg.value, address(0));
+    }
+
+    function donate(
+        uint256 _campaignId,
+        address _token,
+        uint256 _amount
+    ) external {
+        if (!_isCampaignActive(_campaignId)) {
+            revert SupportChildren__CampaignNotActive();
+        }
+        if (_amount == 0) {
+            revert SupportChildren__TokenAmountMustBeGreaterThanZero();
+        }
+
+        Campaign memory campaign = campaigns[_campaignId];
+        if (campaign.wantToken == address(0)) {
+            revert SupportChildren__CampaignWantTokenIsETH();
+        }
+        if (_token != campaign.wantToken) {
+            revert SupportChildren__TokenIsNotWantToken();
+        }
+        if (campaign.receivedAmount + _amount > campaign.hardCap) {
+            revert SupportChildren__CampaignCapIsReached();
+        }
+
+        address beneficiary = campaign.beneficiary;
+
+        // Update the received amount, @research CEI pattern
+        campaigns[_campaignId].receivedAmount += _amount;
+
+        // @hint, you need to approve the contract before calling this function
+        IERC20(_token).transferFrom(msg.sender, beneficiary, _amount); // @research why this is not good practice
+        emit Donation(_campaignId, _amount, _token);
+    }
+
+    function _isCampaignActive(
+        uint256 _campaignId
+    ) internal view returns (bool) {
+        return
+            campaigns[_campaignId].endTimestamp > block.timestamp ||
+            campaigns[_campaignId].hardCap >
+            campaigns[_campaignId].receivedAmount;
+    }
+
+    function _checkCampaignCreationParameters(
+        address _beneficiary,
+        uint256 _endTimestamp,
+        uint256 _hardCap
+    ) internal view {
+        if (_beneficiary == address(0)) {
+            revert SupportChildren__AddressZero();
+        }
+        if (_endTimestamp <= block.timestamp) {
+            revert SupportChildren__CampaignMustEndInFuture();
+        }
+        if (_hardCap == 0) {
+            revert SupportChildren__CapMustBeGreaterThanZero();
+        }
+    }
 }
--- task/solution/src/interfaces/ISupportChildren.sol
+++ task/solution/src/interfaces/ISupportChildren.sol
@@ -1,6 +1,8 @@
 pragma solidity 0.8.26;
 
 interface ISupportChildren {
+    // @research, why this is not best defined struct
+    // @hint struct packing
     struct Campaign {
         uint256 endTimestamp;
         address payable beneficiary;
@@ -12,14 +14,29 @@ interface ISupportChildren {
     }
 
     event CampaignCreated(uint256 campaignId, Campaign campaign);
-    event Donation(uint256 campaignId, uint256 donatedAmountInWantToken, address tokenAddress);
+    event Donation(
+        uint256 campaignId,
+        uint256 donatedAmountInWantToken,
+        address tokenAddress
+    );
+
+    error SupportChildren__CampaignMustEndInFuture();
+    error SupportChildren__AddressZero();
+    error SupportChildren__CapMustBeGreaterThanZero();
+    error SupportChildren__CampaignNotActive();
+    error SupportChildren__EthAmountMustBeGreaterThanZero();
+    error SupportChildren__TokenAmountMustBeGreaterThanZero();
+    error SupportChildren__CampaignCapIsReached();
+    error SupportChildren__CampaignWantTokenIsNotETH();
+    error SupportChildren__FailedToSendEther();
+    error SupportChildren__CampaignWantTokenIsETH();
+    error SupportChildren__TokenIsNotWantToken();
 
     function isCampaignActive(uint256 _campaignId) external view returns (bool);
 
-    function getCampaign(uint256 _campaignId)
-        external
-        view
-        returns (Campaign memory);
+    function getCampaign(
+        uint256 _campaignId
+    ) external view returns (Campaign memory);
 
     function createCampaign(
         address payable _beneficiary,
