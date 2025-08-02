0a6526045ae0fb1aae2bcd67ef58d4113622a652
Nebojša Vuga <nebojsavuga01@gmail.com>
2025-08-02 13:36:15 +0200

    updated logic for withdraw and reate

--- test/src/day-1/StreamingContractV1.sol
+++ test/src/day-1/StreamingContractV1.sol
@@ -1,18 +1,20 @@
 pragma solidity 0.8.24;
 
 import {IStreamingContractV1} from "./IStreamingContractV1.sol";
+import {SafeERC20, IERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+using SafeERC20 for IERC20;
 
 contract StreamingContractV1 is IStreamingContractV1 {
-    // Storage
     uint256 public id;
     mapping(uint256 id => Stream stream) streams;
 
     // Create a new ETH stream (totalAmount comes from msg.value)
-    function createStream(address recipient, uint256 startTime, uint256 endTime)
-        external
-        payable
-        returns (uint256 streamId)
-    {
+    function createStream(
+        address recipient,
+        uint256 startTime,
+        uint256 endTime
+    ) external payable returns (uint256 streamId) {
         if (recipient == address(0)) revert AddressZero();
         if (startTime < block.timestamp) revert MustStartInFuture();
         if (startTime >= endTime) revert InvalidInterval();
@@ -27,10 +29,19 @@ contract StreamingContractV1 is IStreamingContractV1 {
             startTime: startTime,
             endTime: endTime,
             totalAmount: msg.value,
-            withdrawnAmount: 0
+            withdrawnAmount: 0,
+            tokenAddress: address(0),
+            cancelled: false
         });
 
-        emit StreamCreated(streamId, msg.sender, recipient, msg.value, startTime, endTime);
+        emit StreamCreated(
+            streamId,
+            msg.sender,
+            recipient,
+            msg.value,
+            startTime,
+            endTime
+        );
     }
 
     // Withdraw available funds from a stream
@@ -38,17 +49,31 @@ contract StreamingContractV1 is IStreamingContractV1 {
         Stream memory stream = streams[streamId];
         if (streamId >= id) revert InvalidStreamId();
         if (msg.sender != stream.recipient) revert Unauthorized();
-        if (stream.totalAmount == stream.withdrawnAmount) revert AlreadyStreamed();
+        if (stream.totalAmount == stream.withdrawnAmount)
+            revert AlreadyStreamed();
 
-        uint256 withdrawableAmount =
-            _calculateWithdrawableAmount(stream.startTime, stream.endTime, stream.totalAmount, stream.withdrawnAmount);
+        uint256 withdrawableAmount = _calculateWithdrawableAmount(
+            stream.startTime,
+            stream.endTime,
+            stream.totalAmount,
+            stream.withdrawnAmount
+        );
 
         if (withdrawableAmount == 0) revert ZeroToWithdraw();
 
         streams[streamId].withdrawnAmount += withdrawableAmount;
 
-        (bool success,) = stream.recipient.call{value: withdrawableAmount}("");
-        require(success);
+        if (streams[streamId].tokenAddress == address(0)) {
+            (bool success, ) = stream.recipient.call{value: withdrawableAmount}(
+                ""
+            );
+            require(success);
+        } else {
+            IERC20(stream.tokenAddress).safeTransfer(
+                stream.recipient,
+                withdrawableAmount
+            );
+        }
 
         emit Withdrawal(streamId, stream.recipient, withdrawableAmount);
     }
@@ -59,12 +84,20 @@ contract StreamingContractV1 is IStreamingContractV1 {
     }
 
     // Check withdrawable amount
-    function calculateWithdrawableAmount(uint256 streamId) external view returns (uint256) {
+    function calculateWithdrawableAmount(
+        uint256 streamId
+    ) external view returns (uint256) {
         if (streamId >= id) revert InvalidStreamId();
         Stream memory stream = streams[streamId];
-        if (stream.totalAmount == stream.withdrawnAmount) revert AlreadyStreamed();
+        if (stream.totalAmount == stream.withdrawnAmount)
+            revert AlreadyStreamed();
         return
-            _calculateWithdrawableAmount(stream.startTime, stream.endTime, stream.totalAmount, stream.withdrawnAmount);
+            _calculateWithdrawableAmount(
+                stream.startTime,
+                stream.endTime,
+                stream.totalAmount,
+                stream.withdrawnAmount
+            );
     }
 
     function _calculateWithdrawableAmount(
@@ -80,7 +113,48 @@ contract StreamingContractV1 is IStreamingContractV1 {
         } else {
             uint256 timeElapsed = block.timestamp - startTime;
             uint256 duration = endTime - startTime;
-            return (totalAmount * timeElapsed / duration) - withdrawnAmount;
+            return ((totalAmount * timeElapsed) / duration) - withdrawnAmount;
         }
+    }
+
+    function createTokenStream(
+        address recipient,
+        uint256 startTime,
+        uint256 endTime,
+        address tokenAddress,
+        uint256 totalAmount
+    ) external returns (uint256 streamId) {
+        if (recipient == address(0)) revert AddressZero();
+        if (tokenAddress == address(0)) revert AddressZero();
+        if (startTime < block.timestamp) revert MustStartInFuture();
+        if (startTime >= endTime) revert InvalidInterval();
+        if (totalAmount == 0) revert MustStreamAmountGreaterThanZero();
+
+        IERC20(tokenAddress).safeTransferFrom(
+            msg.sender,
+            address(this),
+            totalAmount
+        );
+
+        streamId = id++;
+        streams[streamId] = Stream({
+            recipient: recipient,
+            sender: msg.sender,
+            startTime: startTime,
+            endTime: endTime,
+            totalAmount: totalAmount,
+            withdrawnAmount: 0,
+            tokenAddress: tokenAddress,
+            canceled: false
+        });
+
+        emit StreamCreated(
+            streamId,
+            msg.sender,
+            recipient,
+            totalAmount,
+            startTime,
+            endTime
+        );
     }
 }
